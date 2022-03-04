6ea4c7d4f27a0e8395c35bdfc5c44f588c7d21ac
Pedro Couventaris Daspett <pedrodaspett@hotmail.com>
2022-03-04 03:59:31 -0300

    Adding Bullseye App files

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Pedro Couventaris Daspett on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,56 @@
+  //
+  //  ContentView.swift
+  //  Bullseye
+  //
+  //  Created by Pedro Couventaris Daspett on 03/03/22.
+  //
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  @State private var alertIsVisible : Bool = false
+  @State private var sliderValue : Double = 50.0
+  
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .kerning(2.0)
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+      Text("89")
+        .kerning(-1.0)
+        .fontWeight(.black)
+        .font(.largeTitle)
+      HStack{
+        Text("1")
+          .bold()
+        Slider(value: self.$sliderValue, in: 1.0...100.0)
+        Text("100")
+          .bold()
+      }
+      Button(action: {
+        self.alertIsVisible = true
+      }) {
+        Text("Hit me!")
+      }
+      .alert("Hello there!", isPresented: $alertIsVisible) {
+        Button("Awesome!") { }
+      } message: {
+        var roundedValue: Int = Int(self.sliderValue
+                                      .rounded())
+        Text("The slider's value is \(roundedValue)")
+      }
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+    ContentView()
+      .previewLayout(.fixed(width: 568, height: 320))
+  }
+}
