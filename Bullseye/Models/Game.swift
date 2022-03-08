56f5a5eced1d27bd447ad0ed0e898a2a080e96cd
Pedro Couventaris Daspett <pedrodaspett@hotmail.com>
2022-03-08 18:14:59 -0300

    Changing the target range

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Game {
   var round = 1
   var score = 0
-  var target = Int.random(in: 1...100)
+  var target = Int.random(in: 1..<100)
   
   func points(sliderValue: Int) -> Int {
     100 - abs(sliderValue - target)
